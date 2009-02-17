e60fe0d5c7d0ccc75ed5bea324bc784152d6cba1
vg <gorsunov@gmail.com>
2009-02-17 21:04:51 +0300

    libdizzy

--- t/client.js
+++ t/client.js
@@ -1,51 +1,62 @@
-function l(id) {
-    return document.getElementById(id);
+function L(id) {
+	return document.getElementById(id);
 }
 
+
+function addEl(parent, tagName) {
+	return L(parent).appendChild(document.createElement(tagName));
+}
+
+
+
 var mkWorld = function(url) {
-    var r = {
-	url: url
-    };
-    return r;
+	var r = {
+		url: url
+	};
+	return r;
 };
 
 var getPlayer = function(world, id) {
-    
-
+	var r = {
+		world: world
+	};
+	return r;
 };
 
 // returns world browser data
 var browse = function(worldUrl, container) {
-    var world = mkWorld(worldUrl);
-    var r = {
-	l: l(container),
-	world: world,
-	playerBrowser: browsePlayer( )
-    };
+	var world = mkWorld(worldUrl);
+	var r = {
+		l: L(container),
+		world: world,
+		playerBrowser: browsePlayer(getPlayer(world, getCookie('tplayerid')), 
+																addEl(container, 'DIV')),
+																
+	};
 
-    // build world browser widget
+	// build world browser widget
+	
 
-
-
-    return r;
+	return r;
 };
 
 // returns a player browser struct
-var browsePlayer = function(world, player, container) {
-    var r = {
-	l: l(container),
-	player: player,
-	gameBrowser: browseGame( )
-    };
-
-    // build a player browser widget
-
-
-
-    return r;
+var browsePlayer = function(player, container) {
+	var r = {
+		l: L(container),
+		player: player,
+		gameBrowser: browseGame( )
+	};
+	
+	// build a player browser widget
+	
+	
+	
+	return r;
 };
 
-var browseGame = function(world, player, game, container) {
+var browseGame = function(game, container) {
+	var r = 
     
 };
 	
