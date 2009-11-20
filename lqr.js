cda8d8495fea5649f4cda610c6416c4a78cd27f4
vg <gorsunov@gmail.com>
2009-11-20 21:22:36 +0300

    going home

--- lqr.js
+++ lqr.js
@@ -1,13 +1,59 @@
 function Loc(h) {
 	h = h.split('*');
-	this.xScale = h.shift() || 1;
+	var xScale = h.shift() || 1;
+	var m = xScale.match('([0-9]+)px$');
+	if(m) {
+		this.width  = parseInt(m[1], 10);
+	} else {
+		this.xScale = parseInt(xScale, 10);
+	}
 	this.url = h.join('*');
 }
 
 Loc.prototype.toString = function() {
-	return this.xScale + '*' + this.url;
+	if(this.width) 
+		var s = this.width + 'px';
+	else 
+		var s = this.xScale;
+	return s + '*' + this.url;
 };
 
+Loc.prototype.getWidth = function() {
+	if(this.width)
+		return this.width;
+	if(curImg)
+		return curImg.bs.width * this.xScale;
+	// ?
+}
+
+function render() {
+	if(!loc.url) {
+		ind.innerHTML = 'give me url';
+		return;
+	}
+	
+	ind.innerHTML = 'Rendering...';
+	location.href = '#' + loc.toString();
+	setTimeout(_render, 1);
+}
+
+var curImg;
+function loadImg(url, _handle) {
+	if(curImg && curImg.bs.src == url)
+		return _handle(curImg);
+
+	var im = new Image();
+	im.onload = function() {
+		ind.innerHTML = im.width + 'x' + im.height;
+		curImg = new Img(im);
+		_handle(curImg);
+	};
+
+	ind.innerHTML = 'Loading ' + url + ' ...'; 
+	im.src = url;
+}
+		
+		
 function Img(bs) {
 	this.bs = bs;
 	refCanv.height = bs.height;
@@ -73,30 +119,8 @@ Img.prototype.getSums = function() {
 	return this.sums;
 };
 
-var curImg;
-function loadImg(url, _handle) {
-	if(curImg && curImg.bs.src == url)
-		return _handle(curImg);
 
-	var im = new Image();
-	im.onload = function() {
-		ind.innerHTML = im.width + 'x' + im.height;
-		curImg = new Img(im);
-		_handle(curImg);
-	};
-
-	ind.innerHTML = 'Loading ' + url + ' ...'; 
-	im.src = url;
-}
-		
-		
-
-function render() {
-	if(!loc.url) {
-		ind.innerHTML = 'give me url';
-		return;
-	}
-	
+function _render() {
 	loadImg('/c?' + loc.url, function(img) {
 			var scaledWidth = Math.floor(img.bs.width * loc.xScale);
  			dispCanv.width = dispCanv.style.width = 
@@ -106,21 +130,31 @@ function render() {
 			var c = dispCanv.getContext('2d'), s = '';
 			var dest = c.createImageData(img.bs.width, dispCanv.height);
 			var sums = img.getSums(), d = dest.data;
+			var srcWidth = img.bs.width;
+			var setInd = function(i, val) {
+				if(sums[i] != val) {
+					var minDownVal = energies[i] + sums[i];
+					sums[i] = val;
+					var ni = i + width - 1;
+					
+					setInd(ni++, minDownVal);
+					setInd(ni++, minDownVal);
+					setInd(ni++, minDownVal);
+				}
+			};
+
 			var src = img.imData.data;
+			s = sums.length + ': ' + sums.join(' ');
 
 			var excessColCnt = img.bs.width - scaledWidth;
 			ind.innerHTML = excessColCnt;
+
+			
 			var lastRowStart = sums.length - img.bs.width;
-
-			s = sums.length + ': ' + sums.join(' ');
-
-			//var skip = new Array(excessColCnt * dispCanv.height), 
-			//	skipCnt = 0;
-
 			var visMap = new Array(sums.length);
 
-			for(var visLvl = 1; visLvl <= excessColCnt; visLvl++) {
-				var minInd = Infinity;
+			for(var maskedCnt = 0; maskedCnt < excessColCnt; visLvl++) {
+				var minSum = Infinity;
 				for(var i = sums.length - 1; i >= lastRowStart; i--) {
 					if(!visMap[i]) {
 						if(sums[i] < minSum) {
@@ -129,49 +163,34 @@ function render() {
 						}
 					}
 				}
-				
+				if(Infinity == minSum)
+					break;
 				s += minInd + '=' + minSum + ' ';
 
 				//sums[minInd] = Infinity;
 				var rowStart = lastRowStart;
 				do {
-					visMap[minInd] = visLvl;
-					minInd -= img.bs.width;
-					
-					//rowStart -= img.bs.width;
-					if(minInd < 0)
-						break;
+					setInd(minInd, Infinity);
+					visMap[minInd] = maskedCnt + 1;
 
-									
-					if(sums[minInd+1] < sums[minInd])
-						minInd++;
-					if(sums[minInd+1] < sums[minInd])
-						minInd++;
-					sums[minInd] = Infinity;
-					skip[skipCnt++] = minInd;
 					minInd -= (img.bs.width + 1);
+					//rowStart -= img.bs.width;
+					if(minInd < 0) {
+						if(minInd < -1) 
+							break;
+						minInd = 0;
+					}
+									
+					if(sums[minInd+1] <= sums[minInd]) // kinda prefer to go up
+						minInd++;
+					if(sums[minInd+1] < sums[minInd])
+						minInd++;
 				} while(1);
 			}
 					
 				
-			//for(var i = 0; i < skip.length; i++)
-			//	skip[i] = Math.floor(Math.random() * sums.length);
-
-			
-			// default sort() is lexicographical
-			skip.sort(function(l,r) {
-					if(l < r)
-						return -1;
-					if(l > r)
-						return 1;
-					return 0;
-				});
-
-			//ind.innerHTML = skip.join(' ');
-			skip.push(Infinity);
-
-			for(var i = 0, j = 0, sp = 0, dp = 0; i < sums.length; i++) {
-				if(skip[j] > i) {
+			for(var i = 0, sp = 0, dp = 0; i < sums.length; i++) {
+				if(!visMap[i]) {
 					d[dp++] = sums[i];
 					d[dp++] = sums[i];
 					d[dp++] = sums[i];
@@ -181,8 +200,6 @@ function render() {
 					d[dp++] = src[sp++];
 					d[dp++] = src[sp++];*/
 				} else {
-					//s += i + ":" + j + '=' + skip[j] + '; ';
-					j++;
 					sp += 4;
 					d[dp++] = 0;
 					d[dp++] = 255;
@@ -194,8 +211,7 @@ function render() {
 			dbgInd.innerHTML = s;
 			c.putImageData(dest, 0, 0);
 
-			location.href = '#' + loc.toString();
-			//ind.innerHTML = 'Press keys: a s'
+			ind.innerHTML = 'Press keys: a s'
 		});
 	
 }
@@ -218,22 +234,65 @@ document.onkeydown = function(ev) {
 	break;
 
 	case 65: // A
-	if(loc.xScale > 0.1) { 
-		loc.xScale -= 0.1;
+	case 83: // S
+	if(65 == ev.keyCode)  {
+		var xScale = loc.getXScale() - 0.1;
+	} else {
+		var xScale = loc.getXScale() + 0.1;
+	}
+
+	if(xScale < 0.1) { 
+		xScale = 0.1;
+	} else if(xScale > 5) {
+		xScale = 5;
+	}
+
+	if(xScale != loc.getXScale()) {
+		loc.width = null;
+		loc.xScale = xScale;
 		render();
 	}
 	break;
 
-	case 90: // Z
-	if(loc.xScale < 0.9) { 
-		loc.xScale += 0.1;
+	case 75: // K
+	case 76: // L
+	if(75 == ev.keyCode) 
+		var width = loc.getWidth() - 1;
+	else 
+		var width = loc.getWidth() - 1;
+
+	if(width < 1) 
+		width = 1;
+	else if(width > 2000) 
+		width = 2000;
+
+	if(width != loc.getWidth()) {
+		loc.xScale = null;
+		loc.width = width;
 		render();
-	}
+	}		
 	break;
+
 	}
 };
-	
-var h = location.hash.substring(1);
-var loc = new Loc(h);
-render();
 
+try {
+	var h = location.hash.substring(1);
+	var loc = new Loc(h);
+	render();
+	
+	// bastard onHashChange
+	setInterval(function() {
+			var h = location.hash.substring(1);
+			if(loc.toString() != h) {
+				alert(h + ' != ' + loc.toString());
+				loc = new Loc(h);
+				render();
+			}
+		}, 1000);
+} catch(e) {
+	alert(e);
+}
+
+	
+		
