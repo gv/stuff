c72b7d9e9d5cc0e2f4662acea9ca8bebeefc0236
vg <gorsunov@gmail.com>
2010-05-07 21:36:42 +0400

    something

--- t/game.py
+++ t/game.py
@@ -14,12 +14,6 @@ except ImportError:
 		cjson.encode = cjson.dumps
 		cjson.decode = cjson.loads
 
-print 'Starting server...'
-
-root = resource.Resource()
-root.putChild("", static.File("index.html"))
-root.putChild("m", static.File("m"))
-
 #
 #    MESSAGE EXCHANGE
 #    ``````` ````````
@@ -94,12 +88,10 @@ class Room(Out):
 room = Room()
 room.postMessage(what="createComment", author="Server", text="Started...");
 
+import random
 
 def randomString():
-		s = ""
-		while len(s) < 16:
-				s += chr(randint(48, 123))
-		return s
+		return ''.join(random.choice("poiuytrewqasdfghjklmnbvcxz") for _ in range(16))
 		
 
 class Person(Out):
@@ -107,12 +99,19 @@ class Person(Out):
 				id = randomString()
 				Out.__init__(self, id)
 				self.name = ""
+				self.password = randomString()
+				self.invitations = []
 				room.postMessage(what="createPerson", id=self.id)
 
 		def processMessage(self, m):
 				what = m['what']
 				if what == 'updatePerson':
 						self.name = m['name']
+				elif what == 'createInvitation':
+						self.invitations.append(m)
+
+		def getState(self):
+				return {'invitations': self.invitations}
 				
 
 class BrowserConnection(websocket.WebSocketHandler):
@@ -125,22 +124,60 @@ class BrowserConnection(websocket.WebSocketHandler):
 		def connectionOpened(self):
 				pass
 
+		def send(self, m=None, **kw):
+				m = m or kw
+				self.transport.write(cjson.encode(m))
+
 		def frameReceived(self, frame):
-				m = cjson.decode(frame)
-				print m
-				what = m['what']
-				if what == 'createPerson':
-						self.disown()
-						p = Person()
-						room.connect(self)
-						p.connect(self)
-						self.owner = p
-				elif what == 'createComment':
-						m['author'] = self.owner.id
-						room.postMessage(m)
-				elif what == 'updatePerson':
-						m['id'] = self.owner.id
-						room.postMessage(m)
+				try:
+						m = cjson.decode(frame)
+						print m
+
+						gameId = m.get('game')
+						if gameId:
+								game = self.owner.games[gameId]
+								game.processMessage(m)
+
+						what = m['what']
+						if what in ['createPerson', 'openPerson']:
+								self.disown()
+								p = None
+								if what == 'openPerson':
+										p = outs.get(m['id'])
+										if p and p.password != m['password']:
+												raise ValueError("Bad password")
+								
+								if not p:
+										p = Person()
+								self.send(what="person", id=p.id, password=p.password)
+								room.connect(self)
+								p.connect(self)
+								self.owner = p
+						elif what == 'createComment':
+								m['author'] = self.owner.id
+								room.postMessage(m)
+						elif what == 'updatePerson':
+								m['id'] = self.owner.id
+								room.postMessage(m)
+						elif what == 'createInvitation':
+								target = m['target']
+								if(target == self.owner.id):
+										raise ValueError("It's sort of depressing to invite yourself")
+								target = outs[target]
+								m['src'] = self.owner.id
+								target.postMessage(m)
+						elif what == "createGame":
+								players = m['players']
+								for p in players:
+										if not self.owner.hasInvitation(p):
+												raise ValueError("Invitation from %s not found!" % p)
+								
+								players.append(self.owner.id)
+								Game(players)
+
+				except Exception, e:
+						errMsg = "ERROR: %s" % str(e)
+						self.transport.write(errMsg)
 								
 
 		def connectionLost(self, reason):
@@ -152,21 +189,71 @@ class BrowserConnection(websocket.WebSocketHandler):
 				if self.owner:
 						self.owner.remotes.discard(self)
 						
+def getPerson(id):
+		p = outs.get(id)
+		if p and isinstance(p, Person):
+				return p
+		raise ValueError("No person %s" % id)
 
-site = websocket.WebSocketSite(root)
-site.addHandler("/s", BrowserConnection)
-reactor.listenTCP(8888, site)
 
-try:
-		from twisted.internet import ssl
-		sslContext = ssl.DefaultOpenSSLContextFactory(
-				'privkey.pem', 
-				'cacert.pem',
-				)
-		reactor.listenSSL(443, site, sslContext)
-except ImportError, e:
-		print "No SSL, ", e
 
-print "Running reactor..."
-reactor.run()
+games = {}
+
+class Game:
+		def __init__(self, playerIds):
+				self.id = randomString()
+				self.players = [outs[p] for p in playerIds]
+				games[self.id] = self
+
+				self.turnIndex = 0
+				self.pieces = []
+
+				for p in self.players:
+						p.games[self.id] = self
+						p.postMessage(what="createGame", id=self.id)
+		
+		def getState(self):
+				return {
+						'id': self.id, 
+						'whoseTurn': self.players[self.turnIndex].id,
+						'pieces': self.pieces
+						}
+
+		def processMessage(m):
+				what = m['what']
+				if what == 'createPiece':
+						self.pieces.append(m)
+						# TODO check for victory
+						for p in self.players:
+								p.postMessage(m)
+				
+
+
+
+
+
+
+if __name__ == "__main__":
+		print 'Starting server...'
+
+		root = resource.Resource()
+		root.putChild("", static.File("index.html"))
+		root.putChild("m", static.File("m"))
+
+		site = websocket.WebSocketSite(root)
+		site.addHandler("/s", BrowserConnection)
+		reactor.listenTCP(8888, site)
+
+		try:
+				from twisted.internet import ssl
+				sslContext = ssl.DefaultOpenSSLContextFactory(
+						'privkey.pem', 
+						'cacert.pem',
+						)
+				reactor.listenSSL(443, site, sslContext)
+		except ImportError, e:
+				print "No SSL, ", e
+
+				print "Running reactor..."
+				reactor.run()
 				
--- t/m/c3.js
+++ t/m/c3.js
@@ -20,14 +20,97 @@ function setStatus(s) {
 //    CONNECTION
 //
 
-ins = {};
+
+function Connection() {
+	this.ins = {};
+}
+
+Connection.prototype.connect = function() {
+	var path = "wss://" + location.hostname + "/s";
+	var s = new WebSocket(path), c = this;
+	this.s = s;
+	s.onopen = function(ev) {
+		setStatus("Joining...");
+		var m = document.cookie.match("dontpanic_p=([^;]+)");
+		if(m) {
+			var data = m[1].split('|');
+			m = {
+				what: "openPerson",
+				id: data[0],
+				password: data[1]
+			};
+		} else {
+			var m = {
+				what: "createPerson"
+			};
+		}
+		c.send(m);
+	};
+	
+	s.onmessage = function(ev) { c.onmessage(ev); };
+
+	s.onclose = function(ev) {
+		setStatus("Closed");
+		alert(JSON.stringify(ev));
+		
+	};
+
+	s.onerror = function(ev) {
+		alert(JSON.stringify(ev));
+	};
+};
+
+Connection.prototype.send = function(m) {
+	this.s.send(JSON.stringify(m));
+};
+
+Connection.prototype.onmessage = function(ev) {
+	hideStatus();
+	if(ev.data.charAt(0) == '{') {
+		var m = JSON.parse(ev.data);
+	} else {
+		var m = {errMsg: ev.data};
+	}
+
+	if(m.errMsg) {
+		alert("Connection: " + m.errMsg);
+		return;
+	}
+
+	if("person" == m.what) {
+		document.cookie = "dontpanic_p=" + m.id + '|' + m.password;
+	} else {
+		if(!m.dn) {
+			warn(ev.data + ": No destignation!");
+			return;
+		}
+		
+		var client = this.ins[m.dn];
+		
+		if(!client) { 
+			if(this.person) {
+				alert("SECOND PERSON!");
+			}
+			this.person = client = new In(m.dn);
+			var b = new RoomBrowser();
+			room.addObserver(b);
+			client.addObserver(b);
+		}
+		client.processMessage(m);
+	}
+};
+
+Connection.prototype.areWe = function(id) {
+	return this.person && this.person.id == id;
+};
+
 
 function In(id) {
 	this.id = id;
 	this.version = -1;
 	this.observers = [];
 	this.state = {what: "state"};
-	ins[id] = this;
+	connection.ins[id] = this;
 }
 
 In.prototype.addObserver = function(obs) {
@@ -46,70 +129,15 @@ In.prototype.processMessage = function(m) {
 	}
 }
 
-/*In.prototype.reload = function() {
-	var m = {
-		what: "reload",
-		id: this.id
-	}
-	s.send(JSON.stringify(m));
-};*/
-
-
-function connect() {
-	var path = //location.protocol.replace("http", "ws") + 
-		"wss://" + location.hostname + "/s";
-	//alert(path);
-	s = new WebSocket(path);
-	s.onopen = function(ev) {
-		setStatus("Joining...");
-		var m = {
-			what: "createPerson"
-		};
-		send(m);
-	};
-	
-	s.onmessage = function(ev) {
-		hideStatus();
-		//alert(ev.data);
-		var m = JSON.parse(ev.data);
-		var id = m.dn;
-		if(!id) {
-			warn(ev.data + ": No destignation!");
-			return;
-		}
-		
-		var client = ins[id];
-		
-		if(!client) { // ASSUMING WE DON'T HAVE A CLIENT YET
-			client = new In(id);
-			var b = new RoomBrowser();
-			room.addObserver(b);
-			client.addObserver(b);
-		}
-		client.processMessage(m);
-	};
-	
-	s.onclose = function(ev) {
-		setStatus("Closed");
-		alert(JSON.stringify(ev));
-		
-	};
-
-	s.onerror = function(ev) {
-		alert(JSON.stringify(ev));
-	};
-}
-
-function send(m) {
-	s.send(JSON.stringify(m));
-}
-
 
 //
 //     UI
 //
 
 function RoomBrowser() {
+	this.people = {};
+	this.invitations = [];
+
 	var stage = document.body;
 	this.node = stage.appendChild(document.createElement("DIV"));
 	this.node.className = "roomBrowser";
@@ -120,7 +148,9 @@ function RoomBrowser() {
 	h.innerHTML = "People";
 	this.peopleStage = this.peoplePanel.appendChild(document.createElement("DIV"));
 	this.peopleStage.className = "peopleStage";
-	this.people = {};
+	/*this.invitePanel = this.peoplePanel.appendChild(document.createElement("DIV"));
+	this.inviteBtn = this.invitePanel.appendChild(document.createElement("BUTTON"));
+	this.inviteBtn.innerHTML = "invite";*/
 
 	this.loginPanel = this.node.appendChild(document.createElement("DIV"));
 	this.loginPanel.className = "login";
@@ -140,6 +170,8 @@ function RoomBrowser() {
 	this.conversationStage = this.conversationPanel.appendChild(
 		document.createElement("DIV"));
 	this.conversationStage.className = "comments";
+	this.invitationsStage = this.conversationPanel.appendChild(
+		document.createElement("DIV"));
 
 	this.tribunePanel = this.conversationPanel.appendChild(
 		document.createElement("DIV"));
@@ -154,13 +186,20 @@ function RoomBrowser() {
 
 	var b = this;
 	this.sayBtn.onclick = function() { b.say(); };
-	document.onkeydown = function(ev) {
+	this.sayInp.onkeydown = function(ev) {
 		if(13 == ev.keyCode) 
 			b.say();
 	};
 
 	this.loginBtn.onclick = function() { b.login(); };
+	this.nameInp.onkeydown = function(ev) {
+		if(13 == ev.keyCode)
+			b.login();
+	};
+
 }
+
+			
 
 RoomBrowser.prototype.login = function() {
 	var name = this.nameInp.value;
@@ -170,7 +209,7 @@ RoomBrowser.prototype.login = function() {
 			name: name
 		};
 		setStatus("Sending...");
-		send(m);
+		connection.send(m);
 	}
 };
 
@@ -182,7 +221,7 @@ RoomBrowser.prototype.say = function() {
 			text: text
 		};
 		setStatus("Sending...");
-		send(m);
+		connection.send(m);
 		this.sayInp.value = "";
 	}
 };
@@ -194,7 +233,7 @@ RoomBrowser.prototype.updatePerson = function(p) {
 		return;
 	}
 
-	var node = person.node;
+	var node = person.nameNode;
 	person.name = p.name;
 	node.className = "person";
 	if(p.name) {
@@ -203,6 +242,9 @@ RoomBrowser.prototype.updatePerson = function(p) {
 	} else {
 		node.innerHTML = "*" + p.id + "*";
 	}
+
+	if(connection.person && connection.person.id == p.id)
+		node.className += " you";
 	this.updateConversation();
 };		
 	
@@ -210,6 +252,21 @@ RoomBrowser.prototype.updatePerson = function(p) {
 RoomBrowser.prototype.addPerson = function(p) {
 	p.node = this.peopleStage.appendChild(
 		document.createElement("DIV"));
+	p.nameNode = p.node.appendChild(document.createElement("SPAN"));
+	if(!connection.areWe(p.id)) {
+		p.inviteBtn = p.node.appendChild(document.createElement("BUTTON"));
+		p.inviteBtn.innerHTML = "Invite";
+		var b = this, id = p.id;
+		p.inviteBtn.onclick = function() {
+			var m = {
+				what: "createInvitation",
+				target: id
+			};
+			setStatus("Inviting...");
+			connection.send(m);
+		};
+	}
+
 	this.people[p.id] = p;
 	this.updatePerson(p);
 };
@@ -233,7 +290,45 @@ RoomBrowser.prototype.addComment = function(item) {
 	node.innerHTML = "<b>" + author + "</b>: " + item.text;
 };
 
+RoomBrowser.prototype.createPersonsNameNode = function(id) {
+	var p = this.people[id], n = document.createElement("SPAN");
+	n.className = "person";
+	if(p && p.name) {
+		n.innerHTML = p.name;
+		n.className += " withName";
+	} else {
+		n.innerHTML = "*" + id + "*";
+	}
+
+	if(connection.person && connection.person.id == id) {
+		n.className += " you";
+	}
+	
+	return n;
+}
+
+RoomBrowser.prototype.addInvitation = function(inv) {
+	this.invitations.push(inv);
+	inv.node = this.invitationsStage.appendChild(document.createElement("DIV"));
+	inv.btn = inv.node.appendChild(document.createElement("BUTTON"));
+	var s = inv.btn.appendChild(document.createElement("SPAN"));
+	s.innerHTML = "Play with ";
+	inv.btn.appendChild(this.createPersonsNameNode(inv.src));
+};
+
+
+function TicTacToeBrowser(state) {
+	
+	
+RoomBrowser.prototype.addGame = function(m) {
+	this.games[m.id] = 
+
 RoomBrowser.prototype.processMessage = function(m, client) {
+	if(m.game) {
+		this.games[m.game].processMessage(m, client);
+		return;
+	}
+
 	switch(m.what) {
 	case "state":
 		if("room" == m.dn) {
@@ -251,10 +346,24 @@ RoomBrowser.prototype.processMessage = function(m, client) {
 				this.updateConversation();
 			}
 		} else { // player state here
+			if(m.invitations) {
+				this.invitations = [];
+				this.invitationsStage.innerHTML = "";
+				for(var i in this.invitations)
+					this.addInvitation(this.invitations[i]);
+			}
 
-
-
+			if(m.games) {
+				this.games = {};
+				this.gamesStage.innerHTML = "";
+				for(var id in m.games)
+					this.addGame(m.games[i]);
+			}
 		}
+		break;
+
+	case "createGame":
+		this.addGame(m);
 		break;
 
 	case "createComment":
@@ -269,6 +378,10 @@ RoomBrowser.prototype.processMessage = function(m, client) {
 	case "updatePerson":
 		this.updatePerson(m);
 		break;
+
+	case "createInvitation":
+		this.addInvitation(m);
+		break;
 	}
 }
 
@@ -281,9 +394,9 @@ try {
 	if(!("WebSocket" in window))
 		throw "No WebSockets!";
 	setStatus("Connecting...");
+	connection = new Connection();
 	room = new In("room");
-	connect();
-	//new RoomBrowser();	
+	connection.connect();
 } catch(e) {
 	alert(e);
 }
