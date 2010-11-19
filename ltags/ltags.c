2ba6067b78420a9c470323e173e65b971e97fb2b
unknown <vg@.programmers.kodeks.net>
2010-11-19 19:51:23 +0300

    m m m

--- ltags/ltags.c
+++ ltags/ltags.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+#include <sys/stat.h>
+
 #include "ext/sqlite/sqlite3.h"
 #include "ext/dirent.h"
 
+void debug(const char *s) {
+	fputs(s, stderr);
+}
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName){
   int i;
@@ -15,6 +20,7 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName){
 sqlite3 *db;
 
 void run(char *stmt) {
+	int rc;
   char *zErrMsg = 0;
   rc = sqlite3_exec(db, stmt, callback, 0, &zErrMsg);
   if( rc!=SQLITE_OK ){
@@ -24,16 +30,42 @@ void run(char *stmt) {
   }
 }
 
-#define UP '/' // Let's be consistent with a metaphor of a tree
+// Let's be consistent with a metaphor of a tree
+#define UP '/' 
+
+void updateDir(char *path) {
+	char *end= strrchr(path, 0), *suffix;
+	DIR *d = opendir(path);
+	struct dirent *entry;
+
+	if(!d) {
+		debug("Can't opendir:");
+		debug(path);
+		return;
+	}
 	
-void update(char *rootPath) {
-	char p[MAX_PATH];
-	
+	*end++ = UP;
+	while(entry = readdir(d)) {
+		if('.' == entry->d_name[0])
+			continue;
+		
+		if(S_ISDIR(entry->d_type)) {
+			strncpy(end, entry->d_name, MAX_PATH - (end - path));
+			updateDir(path);
+			continue;
+		}
+
+		strncpy(end, entry->d_name, MAX_PATH - (end - path));
+		suffix = strrchr(end, 0) - 5;
+		if(suffix > path && !stricmp(suffix, ".java")) {
+			puts(path);
+		}
+	}	
 }
 	
 int main(int argc, char **argv){
   int rc;
-
+	char curPath[MAX_PATH];
 
   rc = sqlite3_open(".ltags.sqlite", &db);
   if( rc ){
@@ -44,10 +76,10 @@ int main(int argc, char **argv){
 
 	//run("CREATE VIRTUAL TABLE regions USING rtree(id, start, end)");
 	run("CREATE TABLE IF NOT EXISTS regions("
-		"word, path, mtime, data)");
+		"word, path, mtime, start, end, status)");
 	
-	
-	
+	strcpy(curPath, ".");
+	updateDir(curPath);
   sqlite3_close(db);
   return 0;
 }
--- panes/src/vg/panes/MainScreen.java
+++ panes/src/vg/panes/MainScreen.java
@@ -2,14 +2,27 @@ package vg.panes;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.provider.MediaStore;
+import android.util.Log;
 
 public class MainScreen extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
+	private static final String TAG = "panes";
+	
+	/** Called when the activity is first created. */
+	@Override
     public void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		Intent what = new Intent(Intent.ACTION_PICK, 
+			MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+		startActivityForResult(what, 1);
+	}
+
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		Log.d(TAG, data.getData().toString());
+	}
 }
