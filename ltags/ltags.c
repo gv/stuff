0d9da0347b6cf0092f269d3fbb360968087b2e99
unknown <vg@.programmers.kodeks.net>
2010-11-23 21:09:09 +0300

    reads tokens

--- ltags/ltags.c
+++ ltags/ltags.c
@@ -30,23 +30,230 @@ void run(char *stmt) {
   }
 }
 
-struct Region {
-	char *word;
+struct Span {
+	char *name;
 	int start, end;
+	int mtime;
 	char *path;
 	char *options;
-	struct Region *parent;
+	struct Span *parent;
 	void *particular;
 };
-		
 
-void parseJava(const char *path) {
-	
-
-}
+// parser state		
+struct File {
+	int size;
+	char *contents;
+	struct Span *rootSpan;
+	struct Span *curLeafSpan;
+};
 
 // Let's be consistent with a metaphor of a tree
 #define UP '/' 
+
+// hyperoptimized
+
+#define CHAR_TOKENMIDDLE 1
+#define CHAR_TOKENSTART  2
+#define CHAR_SPACE       4
+
+const int charFlags[/*256*/128] = {
+	4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, // 00
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 10
+	4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, // 20
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, // 30
+	0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, // 40
+	3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 3, 0, // 50
+	0, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, // 60
+	3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 0, 0, 0, 0, 0, // 70
+};
+
+#define countof(something_M) (sizeof(something_M)/sizeof(something_M[0]))
+
+void parseJavaToken(struct File *pFile, const char *start, 
+	const char *end) {
+	char bf[121], *p;
+	p = bf;
+	while(start<end)
+		*p++ = *start++;
+	*p =0;
+	puts(bf);
+
+}
+
+#define SPACE         0
+#define TOKEN         1
+#define NUMBER        2
+#define COMMENT_LINE  '\n'
+#define COMMENT_LINES '*'
+
+void parseJava(const char *path) {
+	FILE *fp;
+	struct File file;
+	struct stat st;
+	int rc;
+	char bf[2 * MAX_PATH], *name, *nameEnd;
+	sqlite3_stmt *stm;
+	int storedTime = 0;
+
+	char *token, *p, *contentsEnd;
+	int mode, flags;
+	
+	rc = stat(path, &st);
+	if(rc < 0) {
+		debug("Can't stat");
+		return;
+	}
+
+	name = strrchr(path, UP);
+	nameEnd = strchr(name, '.');
+	rc = sqlite3_prepare_v2(db, "SELECT mtime FROM spans WHERE name = ?", -1,
+		&stm, 0);
+	if(rc != SQLITE_OK) {
+		fprintf(stderr, "prepare: %d\n", rc);
+		return;
+	}
+
+	rc = sqlite3_bind_text(stm, 1, name, nameEnd - name, SQLITE_TRANSIENT);
+	if(rc != SQLITE_OK) {
+		fprintf(stderr, "bind: %d\n", rc);
+		return;
+	}
+
+	rc = sqlite3_step(stm);
+	if(SQLITE_ROW == rc) {
+		storedTime = sqlite3_column_int(stm, 0);
+	} else if(rc != SQLITE_DONE) {
+		fprintf(stderr, "step: %d\n", rc);
+		return;
+	}
+	
+	if(storedTime == st.st_mtime) {
+		debug("Skipping:");
+		debug(path);
+		return;
+	}
+
+	fp = fopen(path, "r");
+	file.size = st.st_size;
+	file.contents = malloc(st.st_size + 1);
+	if(!file.contents)
+		goto end;
+
+	fread(file.contents, 1, st.st_size, fp);
+	contentsEnd = file.contents + st.st_size;
+	*contentsEnd = '\n'; //padding
+
+	token = p = file.contents;
+	mode = SPACE;
+	flags = 0;
+	for(;;p++) {
+		switch(mode) {
+		case '\'':
+		case '"':
+			// p[-1] is valid here, bc we must have entered this mode by
+			// reading some characters
+			if(mode == *p) {
+				if('\\' != p[-1]) 
+					mode = SPACE;
+			}	else if(contentsEnd == p)
+				goto end;
+			break;
+					
+			
+		case COMMENT_LINE:
+			if('\n' == *p) {
+				if(contentsEnd == p)
+					goto end;
+				mode = SPACE;
+			}
+			break;
+
+		case COMMENT_LINES:
+			if('*' == *p) {
+				// p[1] is valid here, bc last valid byte is padded '\n'
+				if('/' == p[1])
+					mode = SPACE;
+			} else if (contentsEnd == p) 
+				goto end;
+			break;
+
+		case NUMBER:
+			if('.' == *p)
+				break;
+			if(*p >= countof(charFlags))
+				flags = CHAR_TOKENSTART | CHAR_TOKENMIDDLE;
+			else 
+				flags = charFlags[*p];
+			
+			if(flags & CHAR_TOKENMIDDLE) 
+				break;
+			
+			goto space;
+			
+		case TOKEN:
+			if(*p >= countof(charFlags))
+				flags = CHAR_TOKENSTART | CHAR_TOKENMIDDLE;
+			else 
+				flags = charFlags[*p];
+
+			if(flags & CHAR_TOKENMIDDLE) 
+				break;
+			else {
+				// token just completed!
+				parseJavaToken(&file, token, p);
+				mode = SPACE;
+				// no break
+			}
+			
+		case SPACE:
+		space:
+
+			if('/' == *p) {
+				// p[1] is a valid memory reference here because the last byte is 
+				// appended '\n'
+				if('*' == p[1]) {
+					mode = COMMENT_LINES;
+					break;
+				} else if('/' == p[1]) {
+					mode = COMMENT_LINE;
+					break;
+				}
+			}
+
+			if('"' == *p || '\'' == *p) {
+				mode = *p;
+				break;
+			}
+			
+			if(*p >= countof(charFlags))
+				flags = CHAR_TOKENSTART | CHAR_TOKENMIDDLE;
+			else 
+				flags = charFlags[*p];
+			
+			if(CHAR_TOKENSTART & flags) {
+				token = p;
+				mode = TOKEN;
+			} else if(CHAR_TOKENMIDDLE & flags) {
+				mode = NUMBER;
+			} else if(CHAR_SPACE & flags) {
+				if(contentsEnd == p)
+					goto end;
+				break;
+			}
+				
+			// punctuation
+			
+		} // switch(mode)
+	} // for(;;p++)
+		
+		
+	
+ end:
+	if(fp)
+		fclose(fp);
+}
+
 
 void updateDir(char *path) {
 	char *end= strrchr(path, 0), *suffix;
@@ -73,7 +280,7 @@ void updateDir(char *path) {
 		strncpy(end, entry->d_name, MAX_PATH - (end - path));
 		suffix = strrchr(end, 0) - 5;
 		if(suffix > path && !stricmp(suffix, ".java")) {
-			puts(path);
+			parseJava(path);
 		}
 	}	
 }
@@ -90,8 +297,8 @@ int main(int argc, char **argv){
   }
 
 	//run("CREATE VIRTUAL TABLE regions USING rtree(id, start, end)");
-	run("CREATE TABLE IF NOT EXISTS regions("
-		"word, path, mtime, start, end, status)");
+	run("CREATE TABLE IF NOT EXISTS spans("
+		"name, path, mtime, start, end, status)");
 	
 	strcpy(curPath, ".");
 	updateDir(curPath);
