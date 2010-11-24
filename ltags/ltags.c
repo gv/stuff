41b5803834fafec0f97a7a002a00ecf58115d653
unknown <vg@.programmers.kodeks.net>
2010-11-24 21:27:52 +0300

    does something

--- ltags/ltags.c
+++ ltags/ltags.c
@@ -3,10 +3,13 @@
 
 #include "ext/sqlite/sqlite3.h"
 #include "ext/dirent.h"
+#include "ext/getopt/getopt.h"
 
 void debug(const char *s) {
-	fputs(s, stderr);
+	fprintf(stderr, "%s\n", s);
 }
+
+sqlite3 *db;
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName){
   int i;
@@ -16,8 +19,6 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName){
   printf("\n");
   return 0;
 }
-
-sqlite3 *db;
 
 void run(char *stmt) {
 	int rc;
@@ -31,7 +32,7 @@ void run(char *stmt) {
 }
 
 struct Span {
-	char *name;
+	char *name, *nameEnd;
 	int start, end;
 	int mtime;
 	char *path;
@@ -42,12 +43,15 @@ struct Span {
 
 // parser state		
 struct File {
+	char *path;
+	int mtime;
+
 	char *contents;
 	char *contentsEnd;
 	char *token;
 	char *tokenEnd;
 	struct Span *rootSpan;
-	struct Span *curLeafSpan;
+	struct Span *currentSpan;
 	void *langParserState;
 };
 
@@ -73,29 +77,138 @@ const int charFlags[/*256*/128] = {
 
 #define countof(something_M) (sizeof(something_M)/sizeof(something_M[0]))
 
+int loadSpan(struct Span *pSpan) {
+	return 1;
+}
+
+void saveSpan(const struct Span *pSpan) {
+	int r;
+	sqlite3_stmt *stm;
+	static const char *update = "UPDATE spans SET status=0 WHERE " 
+		"name=? AND path=? AND start=? AND end=?",
+		*insert = "INSERT INTO spans (name, path, start, end, mtime, status) "
+		"VALUES (?, ?, ?, ?, ?, 0)";
+	const char *op = update;
+	
+	while(1) {
+		r = sqlite3_prepare_v2(db, op, -1, &stm, 0);
+		if(r != SQLITE_OK) { 
+			fprintf(stderr, "prep (%s): %d\n", op, r);
+			exit(1);
+		}
+
+		r = sqlite3_bind_text(stm, 1, pSpan->name, pSpan->nameEnd - pSpan->name, 
+			SQLITE_STATIC);
+		if(r != SQLITE_OK) { 
+			fprintf(stderr, "bind name: %d\n", r);
+			exit(1);
+		}
+
+		r = sqlite3_bind_text(stm, 2, pSpan->path, -1, 
+			SQLITE_STATIC);
+		if(r != SQLITE_OK) { 
+			fprintf(stderr, "bind path: %d\n", r);
+			exit(1);
+		}
+
+		r = sqlite3_bind_int(stm, 3, pSpan->start);
+		if(r != SQLITE_OK) { 
+			fprintf(stderr, "bind start: %d\n", r);
+			exit(1);
+		}
+
+		r = sqlite3_bind_int(stm, 4, pSpan->end);
+		if(r != SQLITE_OK) { 
+			fprintf(stderr, "bind end: %d\n", r);
+			exit(1);
+		}
+
+		if(insert == op) {
+			r = sqlite3_bind_int(stm, 4, pSpan->mtime);
+			if(r != SQLITE_OK) { 
+				fprintf(stderr, "bind mtime: %d\n", r);
+				exit(1);
+			}
+		}
+			
+
+		// TODO LOCK
+
+		r = sqlite3_step(stm);
+		if(r != SQLITE_DONE) {
+			fprintf(stderr, "step: %d\n", r);
+			exit(1);
+		}
+
+		r = sqlite3_changes(db);
+		
+		// TODO UNLOCK
+
+		if(r)
+			return;
+
+		op = insert;
+	}
+}
+	
+
 struct JavaParserState {
 	char *probableName, *probableNameEnd;
 };
 
-#define THIS ((struct JavaParserState*)pFile->langParserState)
+#define THIS ((struct JavaParserState*)(pFile->langParserState))
 
 static void startParsingJavaSrc(struct File *pFile) {
 	THIS = calloc(sizeof *THIS, 1);
 }
 
-static void parseJavaWord(struct File *pFile) {
-	char bf[121], *p = bf, *start = pFile->token;
-	while(start < pFile->tokenEnd)
+#define NEW 1
+
+static int getJavaReservedWordIndex(char *word, char *end) {
+	if(!strncmp(word, "new", end - word))
+		return NEW;
+	return 0;
+}
+
+struct Span *startSpan(struct File *pf, const char *name, const char *nameEnd,
+	const char *start) {
+	struct Span *s = malloc(sizeof (struct Span));
+	s->name = name;
+	s->nameEnd = nameEnd;
+	s->path = pf->path;
+	s->mtime = pf->mtime;
+	s->start = start - pf->contents;
+	s->end = 0;
+
+	s->parent = pf->currentSpan;
+	pf->currentSpan = s;
+	return s;
+}
+
+struct Span *finishLastSpan(struct File *pf, const char *end) {
+	struct Span *s = pf->currentSpan;
+	s->end = end - pf->contents;
+	saveSpan(s);
+	pf->currentSpan = s->parent;
+	free(s);
+	return pf->currentSpan;
+}
+
+static void parseJavaWord(struct File *pf) {
+	char bf[121], *p = bf, *start = pf->token;
+	while(start < pf->tokenEnd)
 		*p++ = *start++;
 	*p = 0;
 	puts(bf);
+
+	startSpan(pf, pf->token, pf->tokenEnd, pf->token);
+	finishLastSpan(pf, pf->tokenEnd);
 }
 
 static void parseJavaPunctuation(struct File *pFile, const char *p) {
 	struct Span *newSpan;
 	if('{' == *p) {
-		if(probableName) {
-			newSpan = malloc
+	}
 }
 
 
@@ -154,9 +267,22 @@ void parseJava(const char *path) {
 	}
 
 	fp = fopen(path, "r");
-	file.contents = malloc(st.st_size + 1);
-	if(!file.contents)
+	if(!fp) {
+		fprintf(stderr, "Can't fopen %s\n", path);
 		goto end;
+	}
+
+	file.path = path;
+	file.mtime = st.st_mtime;
+
+	file.contents = malloc(st.st_size + 1);
+	if(!file.contents) {
+		fprintf(stderr, "No memory to load %s\n", path);
+		goto end;
+	}
+
+	debug("Loading:");
+	debug(path);
 
 	fread(file.contents, 1, st.st_size, fp);
 	file.contentsEnd = file.contents + st.st_size;
@@ -302,11 +428,11 @@ void updateDir(char *path) {
 		}
 	}	
 }
-	
-int main(int argc, char **argv){
+
+void update() {
   int rc;
 	char curPath[MAX_PATH];
-
+	
   rc = sqlite3_open(".ltags.sqlite", &db);
   if( rc ){
     fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
@@ -316,10 +442,88 @@ int main(int argc, char **argv){
 
 	//run("CREATE VIRTUAL TABLE regions USING rtree(id, start, end)");
 	run("CREATE TABLE IF NOT EXISTS spans("
-		"name, path, mtime, start, end, status)");
+		"name   VARCHAR(100), "
+		"path   VARCHAR(256), "
+		"mtime  INTEGER, "
+		"start  INTEGER, "
+		"end    INTEGER, "
+		"status INTEGER)");
 	
+	debug("Invalidating old entries...");
+	run("UPDATE spans SET status=1"); // 1 means "questionable"
+	run("BEGIN");
 	strcpy(curPath, ".");
 	updateDir(curPath);
+	debug("Commit...");
+	run("COMMIT");
+	debug("Deleting old entries...");
+	run("DELETE FROM spans WHERE status=1");
+
+
   sqlite3_close(db);
+}
+
+const char *dbPath = ".ltags.sqlite";
+
+int main(int argc, char **argv){
+	int r;
+	sqlite3_stmt *stm;
+
+	static struct option longOpts[] = {
+		{0, 0, 0, 0}
+	};
+
+	int c, optInd = 0;
+	while(c = getopt_long(argc, argv, "", longOpts, &optInd), c != -1) {
+
+	}
+
+	if(optind == argc) {
+		update();
+	} else {
+		struct stat st;
+		if(0 > stat(dbPath, &st))
+			update();
+		
+		r = sqlite3_open(dbPath, &db);
+		if( r ){
+			fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+			sqlite3_close(db);
+			exit(1);
+		}
+
+		r = sqlite3_prepare_v2(db, 
+			"SELECT name, path, start, end " 
+			"FROM spans " 
+			"WHERE name = ? "
+			, 
+			-1, &stm, 0);
+		if(r != SQLITE_OK) {
+			fprintf(stderr, "No prepare: %d\n", r);
+			exit(1);
+		}
+
+		r = sqlite3_bind_text(stm, 1, argv[optind], -1, SQLITE_STATIC);
+		if(r != SQLITE_OK) {
+			fprintf(stderr, "No bind: %d\n", r);
+			exit(1);
+		}
+
+		while(r = sqlite3_step(stm), r == SQLITE_ROW) {
+			const char *path, *name;
+			int start, end;
+			name = sqlite3_column_text(stm, 0);
+			path = sqlite3_column_text(stm, 1);
+			start = sqlite3_column_int(stm, 2);
+			end = sqlite3_column_int(stm, 3);
+			printf("%s %s %d %d\n", name, path, start, end);
+		}
+
+		if(r != SQLITE_DONE) {
+			fprintf(stderr, "No step: %d", r);
+			exit(1);
+		}
+	}
+
   return 0;
 }
