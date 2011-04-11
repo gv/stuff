035ec653a8794d1701aca94c7f2c39b2e6a64920
U-rook\vg <vg@rook.(none)>
2011-04-12 03:22:33 +0400

    m

--- memgraph.js
+++ memgraph.js
@@ -17,7 +17,7 @@ MemGraph.prototype.addText = function(text) {
 		var newBaseAddrName = tokens.shift();
 		var newBaseAddr = parseInt(newBaseAddrName);
 		while(baseAddr < newBaseAddr && words.length) {
-			this.wordMap[baseAddr] = words.shift();
+			this.wordMap[baseAddr] = {value: words.shift()};
 			baseAddr += wordLen;
 		}
 		var words = [], baseAddr = newBaseAddr, t;
@@ -27,7 +27,7 @@ MemGraph.prototype.addText = function(text) {
 	}
 
 	while(words.length) {
-		this.wordMap[baseAddr] = words.shift();
+		this.wordMap[baseAddr] = {value: words.shift()};
 		baseAddr += wordLen;
 	}
 
@@ -72,20 +72,58 @@ MemGraph.prototype.render = function() {
 		l.value = y;
 		t.y.baseVal.appendItem(l);
 	};
+
+	var getY = function(a) {
+		return (a - minAddr) * wordHeight / wordLen;
+	};
+
+	svgNode.style.height = getY(maxAddr) - getY(minAddr);
+
+	var wordWidth = 150;
 	
 	for(var k in this.wordMap) {
-		var w = this.wordMap[k];
+		var w = this.wordMap[k].value;
 		var a = parseInt(k, 10);
 		var textTag = createSvgNode("text");
 		textTag.appendChild(document.createTextNode(
 				reprWord(a) + ": " + reprWord(w)));
-		moveText(textTag, 0, (a - minAddr) * wordHeight / this.wordLen);
+		moveText(textTag, 0, getY(a));
 		textTag.style.fill = "#F00";
+
+		if(this.wordMap[w]) {
+			var srcY = getY(a) - wordHeight / 2;
+			var dstY = getY(w) - wordHeight / 2;
+			var p = createSvgNode("path");
+			textTag.style.fill = "#0F0";
+			p.pathSegList.appendItem(p.createSVGPathSegMovetoAbs(0, srcY));
+			p.pathSegList.appendItem(p.createSVGPathSegCurvetoQuadraticAbs(
+					wordWidth, srcY, 
+					wordWidth /2,	srcY - wordHeight / 2));
+			p.pathSegList.appendItem(p.createSVGPathSegCurvetoQuadraticAbs(
+					wordWidth + Math.abs(a - w), (srcY + dstY)/2, 
+					wordWidth + Math.abs(a - w)/2, srcY));
+			p.pathSegList.appendItem(p.createSVGPathSegCurvetoQuadraticSmoothAbs(
+					wordWidth, dstY));
+			/*p.pathSegList.appendItem(p.createSVGPathSegCurvetoQuadraticSmoothAbs(
+			0, dstY));*/
+			st(p, {"stroke-width": 4, stroke: "#100", fill: "transparent"});
+			var back = createSvgNode("rect");
+			back.y.baseVal.value = getY(a) - wordHeight + 4;
+			back.x.baseVal.value = 0;
+			back.height.baseVal.value = wordHeight;
+			back.width.baseVal.value = wordWidth;
+			svgNode.appendChild(textTag); // bring to front 
+		}
 	}
 		
 }
 	
-	
+function st(e, s) {
+	for(k in s) 
+		e.style.setProperty(k, s[k], "");
+	return s;
+}
+
 	
 	
 		
