c39b1a4930d2bd4e8cb6178330825326b008bb8f
unknown <vg@.programmers.kodeks.net>
2011-05-23 19:16:40 +0400

    m

--- sc/src/vg/Sc/SessionList.java
+++ sc/src/vg/Sc/SessionList.java
@@ -11,23 +11,68 @@ import android.content.ServiceConnection;
 import android.content.ComponentName;
 import android.os.IBinder;
 import android.content.Intent;
+import android.util.Log;
+
+import android.hardware.Camera;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
 
 import org.teleal.cling.android.AndroidUpnpService;
 import org.teleal.cling.android.AndroidUpnpServiceImpl;
 
-public class SessionList extends Activity implements NetworkNode.User
-{
+public class SessionList extends Activity 
+	implements NetworkNode.User, SurfaceHolder.Callback {
+	static final String TAG = "DuctTapedC";
 	ServiceConnection m_upnpSvcConn;
+	Camera m_cam;
+	SurfaceHolder m_sfcHl;
+	
+	/* 
+		 Activity events
+	*/
 
 	@Override
-    public void onCreate(Bundle savedInstanceState) 
-	{
+    public void onResume() {
+		try {
+			super.onResume();
+
+			// Init camera
+
+			m_cam = Camera.open();
+			m_cam.startPreview();
+			if(m_sfcHl != null)
+				m_cam.setPreviewDisplay(m_sfcHl);
+		} catch(Exception e) {
+			Log.e(TAG, "onResume", e);
+		}
+	}
+
+
+	@Override
+    public void onPause() {
+		super.onPause();
+		m_cam.release();
+		m_cam = null;
+	}
+
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
 		m_cameras = new ArrayList<NetworkNode.Status>();
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		m_listVw = (ListView) findViewById(R.id.list);
+		SurfaceHolder hl = ((SurfaceView) findViewById(R.id.surface)).getHolder();
+		hl.addCallback(this);
+		hl.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		
 
+
+	/*
+		Init communications
+	*/
+	
 		m_upnpSvcConn = new ServiceConnection() {
 				public void onServiceConnected(ComponentName className, IBinder service) {
 					AndroidUpnpService aus = (AndroidUpnpService) service;
@@ -64,14 +109,42 @@ public class SessionList extends Activity implements NetworkNode.User
 			new Intent(this, AndroidUpnpServiceImpl.class),
 			m_upnpSvcConn,
 			Context.BIND_AUTO_CREATE);
-
-		
-		
 	}
 
-	public void refresh() {
-		
+	@Override protected void onDestroy() {
+		super.onDestroy();
+		if (m_upnpSvcConn != null) {
+			//	upnpService.getRegistry().removeListener(registryListener);
+		}
+		getApplicationContext().unbindService(m_upnpSvcConn);
 	}
+
+	/*
+		Events from SurfaceHolder
+	*/
+
+	public void surfaceCreated(SurfaceHolder h) {
+	}
+
+	public void surfaceChanged(SurfaceHolder hl, int format, int w, int h) {
+		try {
+			m_sfcHl = hl;
+			if(hl.isCreating()) {
+				if(m_cam != null)
+					m_cam.setPreviewDisplay(m_sfcHl);
+			}
+		} catch(Exception e) {
+			Log.e(TAG, "surfaceChanged", e);
+		}			
+	}			
+	
+	public void surfaceDestroyed(SurfaceHolder h) {
+		m_sfcHl = null;
+	}
+	
+	/*
+		Events from NetworkNode
+	*/
 
 	public void handleOtherNodePresence(NetworkNode.Status stat) {
 		if(!m_cameras.contains(stat))
@@ -84,6 +157,15 @@ public class SessionList extends Activity implements NetworkNode.User
 		redisplayList();
 	}
 
+
+	/*
+		Utils
+	*/
+
+	private void refresh() {
+		
+	}
+
 	private void redisplayList() {
 		runOnUiThread(new Runnable() {
 				public void run() {
@@ -93,14 +175,6 @@ public class SessionList extends Activity implements NetworkNode.User
 							m_cameras.toArray(new NetworkNode.Status[0])));
 				}
 			});
-	}
-
-	@Override protected void onDestroy() {
-		super.onDestroy();
-		if (m_upnpSvcConn != null) {
-			//	upnpService.getRegistry().removeListener(registryListener);
-		}
-		getApplicationContext().unbindService(m_upnpSvcConn);
 	}
 
 	private ListView m_listVw;
