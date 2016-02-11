ddd38f00f677a701b83c208f6d1e3250b50af224
vg <vg@gx.work.kodeks.ru>
2016-02-11 12:24:55 +0300

    .backup

--- elisp/shoe.el
+++ elisp/shoe.el
@@ -30,7 +30,7 @@
 ;;
 (setq
    backup-by-copying t      ; don't clobber symlinks
-   backup-directory-alist '(("." . "~/backup"))    ; don't litter my fs tree
+   backup-directory-alist '(("." . "~/.backup"))    ; don't litter my fs tree
    delete-old-versions t
    kept-new-versions 6
    kept-old-versions 2
